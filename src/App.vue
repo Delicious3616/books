a87d39681bd41e73584cb32599b9ffbe64f97e78
haitao <haitao3616@163.com>
2017-09-22 17:40:02 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h1>{{title}}</h1>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li v-for = 'todo in todos'>
+          <label>
+            {{todo.value}}
+          </label>
+      </li>
     </ul>
   </div>
 </template>
@@ -24,37 +16,17 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to You77777r666666 Vue.js App'
+      title:"vue-todos",
+      todos:[
+        {value:"阅读一本关于前端开发的书",done:false},
+        {value:"补充范例模板",done:true},
+        {value:"阅读一本关于前端开发的书",done:false}
+      ]
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
