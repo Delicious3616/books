d483c3295ee898c9c66baa8013cbfc20094ec198
haitao2 <1013855276@qq.com>
2017-09-23 12:08:09 +0800

    add filters

--- src/App.vue
+++ src/App.vue
@@ -8,24 +8,35 @@
           <label>
             {{index+1}}.{{todo.value}}
           </label>
+          <time>{{todo.created | date}}</time>
       </li>
     </ul>
+
   </div>
 </template>
 
 <script>
 import './assets/todos.less'
 import './assets/site.less'
+import moment from 'moment'
+import 'moment/locale/zh-cn'
+moment.locale('zh-cn')
+
 export default {
   name: 'app',
   data () {
     return {
       title:"vue-todos",
       todos:[
-        {value:"阅读一本关于前端开发的书888",done:false},
-        {value:"补充范例模板",done:true},
-        {value:"阅读一本关于前端开发的书2",done:false}
+        {value:"阅读一本关于前端开发的书",done:false,created:Date.now()},
+        {value:"补充范例模板",done:true,created:Date.now()+300000},
+        {value:"阅读一本关于前端开发的书",done:false,created:Date.now()-300000}
       ]
+    }
+  },
+  filters:{
+    date(val){
+      return moment(val).calendar()
     }
   }
 }
